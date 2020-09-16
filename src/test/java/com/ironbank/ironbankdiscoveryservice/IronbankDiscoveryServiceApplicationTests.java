635bf1d32b32420ea17f01b74d0bd3bc7a0f1e28
jeka1978 <kit2009>
2020-09-17 02:19:37 +0300

    Initial commit

---
+++ src/test/java/com/ironbank/ironbankdiscoveryservice/IronbankDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironbank.ironbankdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronbankDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
